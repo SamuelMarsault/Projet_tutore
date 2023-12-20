8f14196ae76d5d093f3746ef4929b3d0056aed0e
Samuel Marsault <marsaults21@gmail.com>
2023-12-20 16:14:28 +0100

    Ajout menu de démarrage

--- TerritoriaV1/GameManager.cs
+++ TerritoriaV1/GameManager.cs
@@ -6,29 +6,35 @@ using TerritoriaV1;
 public partial class GameManager : Node2D
 {
 	private VillageManager villageManager;
-	EvolutionOfVillage evolutionOfVillage;
+	private EvolutionOfVillage evolutionOfVillage;
 
 	turnNB turn;
-
 	int nbMaxTurn = 50;
 	int currentTurnNb = 1;
+
+	private Printer print;
+	private Trader trade;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		turn = GetNode<turnNB>("t");
 		turn.updateCurrentTurn(1);
+		turn.Visible = false;
 	
 		MissingRessource missingResource = GetNode<MissingRessource>("MissingRessource");
 		var printer = GetNode<Printer>("Printer");
 		printer.setMessageWindow(missingResource);	
+
+		var trader = GetNode<Trader>("Trader");
 		
 		evolutionOfVillage = new EvolutionOfVillage(this);
 		if(evolutionOfVillage != null)
 	
-		villageManager = new VillageManager(GetNode<TileMap>("Map"),printer,GetNode<Trader>("Trader"),evolutionOfVillage);		
-		
-		printMessage("bienvenue, vous êtes responsables de l'import et de l'export des ressources de notre village. nous comptons sur vous");
+		villageManager = new VillageManager(GetNode<TileMap>("Map"),printer,trader,evolutionOfVillage);	
+
+		this.print = printer;	
+		this.trade = trader;
 	}
 	
 	public void nextTurn(int[] export, int[] import, int[] money)
@@ -78,6 +84,19 @@ public partial class GameManager : Node2D
 		var messageDialog = new MessageDialog();
 		messageDialog.SetErrorMessage(message);
 		AddChild(messageDialog);
-		messageDialog.PopupCenteredClamped();
+		messageDialog.PopupCentered();
+	}
+
+	public void _on_start_pressed(){
+		var menu = GetNode<TextureRect>("StartMenu");
+		this.trade.setVisibility();
+		this.print.setVisibility();
+		turn.Visible = true;
+		menu.Visible = false;
+		printMessage("bienvenue, vous êtes responsables de l'import et de l'export des ressources de notre village. nous comptons sur vous");
+	}
+
+	public void _on_exit_pressed(){
+		GetTree().Quit();
 	}
 }
