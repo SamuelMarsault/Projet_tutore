64ee18b7073a94ea6bd6b2d9ec884f5287451393
Samuel Marsault <marsaults21@gmail.com>
2023-12-21 15:14:47 +0100

    Suppression code inutile

--- TerritoriaV1/Printer.cs
+++ TerritoriaV1/Printer.cs
@@ -91,43 +91,6 @@ public partial class Printer : Node, VillageObserver
 		
 	}
 
-	/*public void ReactToImpossibleTransaction(int[] missingRessources)
-	{
-		// Créez une instance de la fenêtre de dialogue
-
-		string message = "Vous n'avez pas assez de ressources, il vous manque : \n";
-
-		for (int i = 0; i<missingRessources.Length ; i++){
-			if (missingRessources[i] != 0){
-				switch(i){
-					case 0:
-						message = message+"	\u2022 "+missingRessources[i]+" de bois\n";
-						break;
-					case 1:
-						message = message+"	\u2022 "+missingRessources[i]+" de hoop\n";
-						break;
-					case 2:
-						message = message+"	\u2022 "+missingRessources[i]+" de glace\n";
-						break;
-					case 3:
-						message = message+"	\u2022 "+missingRessources[i]+" de bière\n";
-						break;
-					case 4:
-						message = message+"	\u2022 "+missingRessources[i]+" d'argent\n";
-						break;
-					default:
-						break;
-				}
-			}
-		}
-
-		// Définissez le message d'erreur
-		windowMissingRessource.SetMessageMissingRessource(message);
-
-		windowMissingRessource.PopupCentered();
-
-	}*/
-
 	public void ReactToImpossibleTransaction(int[] missingRessources)
 	{
 		// Créez une instance de la fenêtre de dialogue
--- TerritoriaV1/Trader.cs
+++ TerritoriaV1/Trader.cs
@@ -59,13 +59,14 @@ public partial class Trader : Node, VillageObserver
 
 	public void ReactToPlaceableChange(Placeable[,] placeables)
 	{
-		
+		return;
 	}
 
 	public void ReactToTilesChange(TileType[,] tiles)
 	{
-		
+		return;
 	}
+
 	private void _on_button_pressed()
 	{
 		int[] import = new int[Enum.GetNames(typeof(ResourceType)).Length-1];
@@ -78,20 +79,6 @@ public partial class Trader : Node, VillageObserver
 			import[i] = resourceTradeUnits[i].GetImportValue();
 			money[i] -= resourceTradeUnits[i].GetImportValue() * exchangesRates[0,i];
 		}
-		/*
-		for (int i = 0; i < export.Length; i++)
-		{
-			GD.Print("export : "+export[i]);
-		}
-		for (int i = 0; i < import.Length; i++)
-		{
-			GD.Print("import : "+import[i]);
-		}
-		for (int i = 0; i < import.Length; i++)
-		{
-			GD.Print("money : "+money[i]);
-		}
-		*/
 		parent.nextTurn(export, import, money);
 	}
 
@@ -106,7 +93,6 @@ public partial class Trader : Node, VillageObserver
 		for (int i = 0; i < resourceTradeUnits.Count; i++)
 		{
 			int[] newRates = { exchangesRates[0,i],exchangesRates[1,i] };
-			//Console.WriteLine("coucou : "+this.exchangesRates[1,i]+" "+exchangesRates[0,i]);
 			resourceTradeUnits[i].SetExchangeRate(newRates);
 		}
 	}
